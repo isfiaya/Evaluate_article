17b18d8cd201d978672ac700d6b5746034883180
aymen isfiaya <aymen.isfiaya@gmail.com>
2021-10-18 15:50:21 +0200

    add: service workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,8 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 module.exports = {
   entry: './src/client/index.js',
   mode: 'production',
@@ -33,5 +35,6 @@ module.exports = {
       filename: "./index.html",
     }),
     new MiniCssExtractPlugin({ filename: "[name].css" }),
+    new WorkboxPlugin.GenerateSW()
   ]
 }
